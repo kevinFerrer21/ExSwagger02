6666a540029de0cfb3e900bab6fca37a129cb656
Kevin <kebbo2110@gmail.com>
2023-01-25 10:23:18 +0100

    first commit

---
+++ src/main/java/co/develhope/swagger2/controllers/DefaultController.java
@@ -0,0 +1,16 @@
+package co.develhope.swagger2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping
+    public String getWelcomeMessage() {
+
+        return "Welcome";
+
+    }
+
+}
